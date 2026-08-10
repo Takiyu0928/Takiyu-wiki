0c305c35d2088109290436e33cef3c0b544e9efb
Takiyu <erenaaa630@gmail.com>
2026-08-10 22:59:04 +0800

    Update navBarConfig.ts

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -141,21 +141,15 @@ export const navBarConfig: NavBarConfig = {
 			children: [
 				{
 					name: "GitHub",
-					url: "https://github.com/LyraVoid/Mizuki",
+					url: "https://github.com/Takiyu0928/Takiyu-wiki",
 					external: true, // 外部链接，新标签页打开
 					icon: "fa7-brands:github",
 				},
 				{
 					name: "Bilibili",
-					url: "https://space.bilibili.com/701864046",
+					url: "https://space.bilibili.com/1590135171",
 					external: true,
 					icon: "fa7-brands:bilibili",
-				},
-				{
-					name: "Gitee",
-					url: "https://gitee.com/matsuzakayuki/Mizuki",
-					external: true,
-					icon: "mdi:git",
 				},
 			],
 		},
