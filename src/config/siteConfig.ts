7f7f3ef139e7782292954e66a9625e64e5785f09
Takiyu <erenaaa630@gmail.com>
2026-08-12 21:23:20 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -5,7 +5,7 @@ const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
 	title: "Takiyu's blog",
-	subtitle: "One demo website",
+	subtitle: "my demo website",
 	siteURL: "https://takiyu-wiki.ccwu.cc/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-08-04", // 站点开始运行日期，用于站点统计组件计算运行天数
 
@@ -34,7 +34,7 @@ export const siteConfig: SiteConfig = {
 		// 显示模式："text-icon" 显示图标+文本，"logo" 仅显示Logo
 		mode: "text-icon",
 		// 顶栏标题文本
-		text: "Takiyu",
+		text: "TakiyuUI",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
 		icon: "assets/home/wiki-icon.webp",
 		// 网站Logo图片路径
