8629d44afd6e3cabf5b9249739888465c51bc07f
Takiyu <takiyu0928@gmail.com>
2026-08-19 21:14:03 +0800

    Update devices.ts

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -17,7 +17,7 @@ export const devicesData: DeviceCategory = {
 	Xiaomi: [
 		{
 			name: "Xiaomi 17ProMax",
-			image: "images/device/Xiaomi%2017ProMax.png",
+			image: "/images/device/Xiaomi%2017ProMax.png",
 			specs: "White / 16GB + 512GB",
 			link: "https://www.mi.com/prod/xiaomi-17-pro-max",
 		},
@@ -25,7 +25,7 @@ export const devicesData: DeviceCategory = {
 	LapTop: [
 		{
 			name: "ROG G615",
-			image: "public/images/device/ROG-StrixG16.png",
+			image: "/images/device/ROG-StrixG16.png",
 			specs: "U9-275hx 5070ti",
 			link: "https://rog.asus.com.cn/laptops/rog-strix/rog-strix-g16-2025/",
 		},
